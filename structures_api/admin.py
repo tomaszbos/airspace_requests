0792ce3539825b9fadcd397a4d1f55e08c7f1546
Tomasz Boś <tomaszbos@gmail.com>
2020-10-13 18:36:26 +0200

    Admin functionality

--- structures_api/admin.py
+++ structures_api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import AirspaceStructure
+
+
+@admin.register(AirspaceStructure)
+class AirspaceStructuresAdmin(admin.ModelAdmin):
+    list_display = ('name', 'airspace_type')
+    site_header = 'Airspace requests and structures administration'
