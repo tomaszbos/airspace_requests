e091b0628b01aa21033266d22964cac204a6dce3
Tomasz Boś <tomaszbos@gmail.com>
2020-10-16 16:39:21 +0200

    Update admin

--- structures_api/admin.py
+++ structures_api/admin.py
@@ -5,5 +5,8 @@ from .models import AirspaceStructure
 
 @admin.register(AirspaceStructure)
 class AirspaceStructuresAdmin(admin.ModelAdmin):
-    list_display = ('name', 'airspace_type')
-    site_header = 'Airspace requests and structures administration'
+    ordering = ['name']
+    search_fields = ['name', 'airspace_type']
+    list_display = ('name', 'airspace_type', 'lower_limit', 'upper_limit')
+    admin.site.site_header = 'Airspace application administration'
+    admin.site.index_title = 'Authentication, Authorization and Airspace Structures administration'
